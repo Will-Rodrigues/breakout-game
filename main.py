11deabf91aa65e68f3755e38965ddeb410265c82
William Gabriel <williamgabriel.rl@aol.com>
2022-08-25 08:21:48 -0300

    Add Screen function in main.py

--- main.py
+++ main.py
@@ -4,4 +4,17 @@
 # TODO: score
 # TODO: start menu
 
-# -------------------------------------
+# -------------------------
+from turtle import Screen
+
+screen = Screen()
+screen.setup(800, 600)
+screen.title('Breakout')
+screen.tracer(0)
+
+is_on = True
+
+while is_on:
+    screen.update()
+
+screen.exitonclick()
